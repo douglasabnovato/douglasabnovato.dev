d2fc182758eab04126de6924db5f24436360290f
Douglas A B Novato <douglasabnovato@gmail.com>
2022-03-03 11:41:04 -0300

    consumindo a api

--- main.js
+++ main.js
@@ -1,29 +1,44 @@
-function getGithub(url){
+document.body.onload = adcElemento;
+
+function adcElemento () {
+
+  // cria um novo elemento div e dá à ele conteúdo
+  var divNova = document.createElement("div");
+  var conteudoNovo = document.createTextNode("Olá, cumprimentos!");
+
+  //adiciona o nó de texto à nova div criada
+  divNova.appendChild(conteudoNovo); 
+
+  // adiciona o novo elemento criado e seu conteúdo ao DOM
+  var divAtual = document.getElementById("div1");
+  document.body.insertBefore(divNova, divAtual);
+
+}
+
+function getApi(url){
     let request = new XMLHttpRequest()
-
-    request.open("GET", url, false)
+    request.open("GET", url,false)
     request.send()
-
     return request.responseText
 }
 
-function getRepositorio(user){
-
-
+function createRepository(user){
 
 }
 
 function main(){
+    data = getApi("https://api.github.com/users/douglasabnovato/repos")
+    users = JSON.parse(data)
 
-    let data = getGithub("https://api.github.com/users/douglasabnovato/repos")
-    let users = JSON.parse(data)    
-    let tablerepos = document.getElementById("tablerepos")
-
-    users.forEach(element => {
-        let repository = getRepositorio(element)
-        tablerepos.appendChild(repository)
-    })
-
+    for(let i=0; i < users.length; i++){        
+        console.log("Repository: ",i)
+        console.log(users[i].name)
+        console.log(users[i].description)
+        console.log(users[i].stargazers_count)
+        console.log(users[i].forks_count)
+        console.log(users[i].language) 
+    }
+    
 }
 
-main() 
+main()
