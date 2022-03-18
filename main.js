482654702b1f1d72ec05f16468107d33ddb28913
Douglas A B Novato <douglasabnovato@gmail.com>
2022-03-18 19:06:22 -0300

    os cards dos projetos estão com um link para a raiz dos respectivos repositorios

--- main.js
+++ main.js
@@ -9,72 +9,79 @@ function getApi(url_github){
 
 function createRepo(repository){
 
+        console.log(repository)
+
         const div_project_cards = document.createElement('div')
         div_project_cards.setAttribute('class', 'project cards')
 
-        const div_fragment = document.createElement('div')
-        div_fragment.setAttribute('class', 'fragment')
+        const a_repos = document.createElement('a')
+        a_repos.setAttribute('href', repository.html_url)
+        a_repos.setAttribute('target', '_blank')
 
-        const div_title_project = document.createElement('div')
-        div_title_project.setAttribute('class', 'title-project')
+            const div_fragment = document.createElement('div')
+            div_fragment.setAttribute('class', 'fragment')
 
-        const span_icons_folder = document.createElement('span')
-        span_icons_folder.setAttribute('class', 'icons-folder')
+            const div_title_project = document.createElement('div')
+            div_title_project.setAttribute('class', 'title-project')
 
-        const span_title = document.createElement('span')
-        span_title.innerText = repository.name
+            const span_icons_folder = document.createElement('span')
+            span_icons_folder.setAttribute('class', 'icons-folder')
 
-        div_title_project.appendChild(span_icons_folder)
-        div_title_project.appendChild(span_title)
+            const span_title = document.createElement('span')
+            span_title.innerText = repository.name
 
-        const div_description_project = document.createElement('div')
-        div_description_project.setAttribute('class', 'description-project')
-        div_description_project.innerText = repository.description
+            div_title_project.appendChild(span_icons_folder)
+            div_title_project.appendChild(span_title)
 
-        const div_details_project = document.createElement('div')
-        div_details_project.setAttribute('class', 'details-project')
+            const div_description_project = document.createElement('div')
+            div_description_project.setAttribute('class', 'description-project')
+            div_description_project.innerText = repository.description
 
-        const div_stars_branches = document.createElement('div')
-        div_stars_branches.setAttribute('class', 'stars-branches')
+            const div_details_project = document.createElement('div')
+            div_details_project.setAttribute('class', 'details-project')
 
-        const div_stars = document.createElement('div')
-        const span_icons_star = document.createElement('span')
-        span_icons_star.setAttribute('class', 'icons-star')
+            const div_stars_branches = document.createElement('div')
+            div_stars_branches.setAttribute('class', 'stars-branches')
 
-        const span_star = document.createElement('span')
-        span_star.innerText = repository.stargazers_count
-        div_stars.appendChild(span_icons_star)
-        div_stars.appendChild(span_star)
+            const div_stars = document.createElement('div')
+            const span_icons_star = document.createElement('span')
+            span_icons_star.setAttribute('class', 'icons-star')
 
-        const div_gitbranch = document.createElement('div')
-        const span_icons_gitbranch = document.createElement('span')
-        span_icons_gitbranch.setAttribute('class', 'icons-gitbranch')
+            const span_star = document.createElement('span')
+            span_star.innerText = repository.stargazers_count
+            div_stars.appendChild(span_icons_star)
+            div_stars.appendChild(span_star)
 
-        const span_gitbranch = document.createElement('span')
-        span_gitbranch.innerText = repository.forks_count
-        div_gitbranch.appendChild(span_icons_gitbranch)
-        div_gitbranch.appendChild(span_gitbranch)
+            const div_gitbranch = document.createElement('div')
+            const span_icons_gitbranch = document.createElement('span')
+            span_icons_gitbranch.setAttribute('class', 'icons-gitbranch')
 
-        div_stars_branches.appendChild(div_stars)
-        div_stars_branches.appendChild(div_gitbranch)
+            const span_gitbranch = document.createElement('span')
+            span_gitbranch.innerText = repository.forks_count
+            div_gitbranch.appendChild(span_icons_gitbranch)
+            div_gitbranch.appendChild(span_gitbranch)
 
-        const div_languages = document.createElement('div')
-        const span_icons_languages = document.createElement('span')
-        span_icons_languages.setAttribute('class', 'icons-languages')
+            div_stars_branches.appendChild(div_stars)
+            div_stars_branches.appendChild(div_gitbranch)
 
-        const span_languages = document.createElement('span')
-        span_languages.innerText = repository.language
-        div_languages.appendChild(span_icons_languages)
-        div_languages.appendChild(span_languages)
+            const div_languages = document.createElement('div')
+            const span_icons_languages = document.createElement('span')
+            span_icons_languages.setAttribute('class', 'icons-languages')
 
-        div_details_project.appendChild(div_stars_branches)
-        div_details_project.appendChild(div_languages)
+            const span_languages = document.createElement('span')
+            span_languages.innerText = repository.language
+            div_languages.appendChild(span_icons_languages)
+            div_languages.appendChild(span_languages)
 
-        div_fragment.appendChild(div_title_project)
-        div_fragment.appendChild(div_description_project)
-        div_fragment.appendChild(div_details_project)
+            div_details_project.appendChild(div_stars_branches)
+            div_details_project.appendChild(div_languages)
 
-        div_project_cards.appendChild(div_fragment)
+            div_fragment.appendChild(div_title_project)
+            div_fragment.appendChild(div_description_project)
+            div_fragment.appendChild(div_details_project)
+
+        a_repos.appendChild(div_fragment)
+        div_project_cards.appendChild(a_repos)
 
         return div_project_cards
 
